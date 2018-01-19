44f860f7952e1aa7d9510614d906d8db275093a3
LunarLite <mdtozer@hotmail.com>
2018-01-19 13:40:08 +0100

    Temporary force based graph added

--- application/js/visualizer.js
+++ application/js/visualizer.js
@@ -58,6 +58,60 @@ function structure_visualisation()
 			
 		console.log(commitHistory);
 		console.log(structureData);
+			
+		var graph = {
+		  nodes: d3.range(13).map(Object),
+		  links: [
+			{source:  0, target:  1},
+			{source:  1, target:  2},
+			{source:  2, target:  0},
+			{source:  1, target:  3},
+			{source:  3, target:  2},
+			{source:  3, target:  4},
+			{source:  4, target:  5},
+			{source:  5, target:  6},
+			{source:  5, target:  7},
+			{source:  6, target:  7},
+			{source:  6, target:  8},
+			{source:  7, target:  8},
+			{source:  9, target:  4},
+			{source:  9, target: 11},
+			{source:  9, target: 10},
+			{source: 10, target: 11},
+			{source: 11, target: 12},
+			{source: 12, target: 10}
+		  ]
+		};
+
+		var force = d3.layout.force()
+			.nodes(graph.nodes)
+			.links(graph.links)
+			.size([svgWidth, svgHeight])
+			.charge(-200)
+			.on("tick", tick)
+			.start();
+
+		var link = parentSvg.selectAll(".link")
+		   .data(graph.links)
+		 .enter().append("line")
+		   .attr("class", "link");
+
+		var node = parentSvg.selectAll(".node")
+		   .data(graph.nodes)
+		 .enter().append("circle")
+		   .attr("class", "node")
+		   .attr("r", 4.5)
+		   .style("fill", "blue	");
+
+		function tick() {
+		  link.attr("x1", function(d) { return d.source.x; })
+			  .attr("y1", function(d) { return d.source.y; })
+			  .attr("x2", function(d) { return d.target.x; })
+			  .attr("y2", function(d) { return d.target.y; });
+
+		  node.attr("cx", function(d) { return d.x; })
+			  .attr("cy", function(d) { return d.y; });
+		}
 	});
 
 }
@@ -215,7 +269,10 @@ function growth_visualisation()
 			
 		var positiveLine = d3.svg.line()
 			.x(function(d, i) { return (i / codeFrequencyData.length) * growthDataWidth + padding;})
-			.y(function(d) { return -((d["1"] / maxCodeValue) * growthDataHeight / 2) + (growthDataHeight / 2); })
+			.y(function(d) 
+			{ 
+				return (-((d["1"] / maxCodeValue) * growthDataHeight / 2) + (growthDataHeight / 2)) - 1; 
+			})
 			.interpolate('linear');
 
 		var positiveLines = codeFrequencyDataSvg.append("path")
@@ -226,7 +283,10 @@ function growth_visualisation()
 			
 		var negativeLine = d3.svg.line()
 			.x(function(d, i) { return (i / codeFrequencyData.length) * growthDataWidth + padding;})
-			.y(function(d) { return -((d["2"] / maxCodeValue) * growthDataHeight / 2) + (growthDataHeight / 2); })
+			.y(function(d) 
+			{ 
+				return (-(((d["2"]) / maxCodeValue) * growthDataHeight / 2) + (growthDataHeight / 2)) + 1; 
+			})
 			.interpolate('linear');
 
 		var negativeLines = codeFrequencyDataSvg.append("path")
